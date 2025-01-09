95d76d84283d8e50890cb121001fb61be2c0fa09
Ashiq Ahamed <aashiq121205@gmail.com>
2025-01-09 15:36:50 +0530

    update

---
+++ JavaScript/index.js
@@ -0,0 +1,21 @@
+
+console.log("Bharath AKA OneShot");
+
+
+const obj = {
+    name: "Barath",
+    age: 19,
+    address: {
+        city: "NYC",
+        address: "123, Time Square"
+    }
+}
+console.log(JSON.stringify(obj));
+
+const obj2 = new Object();
+obj2.name = "As";
+console.log(obj2)
+const sett = new setImmediate("A", 2, 5,6);
+clg
+
+
