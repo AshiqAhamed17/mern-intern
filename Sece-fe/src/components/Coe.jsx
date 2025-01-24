4f29c4567d8e7e223954b552dffc432ac9065a9b
Ashiq Ahamed <aashiq121205@gmail.com>
2025-01-24 16:00:03 +0530

    Day 11

---
+++ Sece-fe/src/components/Coe.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Exam form "./Exam.jsx";
+function Coe({props}) {
+  return (
+    <div>
+      <h3>  Paper correction is in process</h3>
+      <h3> the result is </h3>
+    </div>
+  )
+}
+
+export default Coe
---
+++ Sece-fe/src/components/Exam.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Exam() {
+    const mark = 98;
+  return (
+    <div>
+      <h2> </h2>
+    </div>
+  )
+}
+
+export default Exam
