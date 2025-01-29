17a9cb4568c75266dab72cd1c6c8b160dd4a4187
Ashiq Ahamed <aashiq121205@gmail.com>
2025-01-29 08:47:43 +0530

    Task: Login page

--- seceBackend/index.js
+++ seceBackend/index.js
@@ -3,15 +3,17 @@ const path=require('path')
 const mdb=require("mongoose")
 const dotenv=require("dotenv")
 const Signup=require("./models/signupSchema")
+const bcrypt=require("bcrypt")
+const cors=require("cors")
 const app=express()
 dotenv.config();
-
+app.use(cors());
 app.use(express.urlencoded());
 app.use(express.json())
 mdb.connect(process.env.MONGODB_URL).then(()=>{
     console.log("connected successfully")
 }).catch((err)=>{
-    console.log("not connected")
+    console.log(err)
 })
 
 app.get('/',(req,res)=>{
@@ -25,21 +27,23 @@ app.get('/newPath2',(req,res)=>{
     res.json({"key":"index.html"});
 })
 
-app.post("/signup", (req,res)=>{
+app.post("/signup", async(req,res)=>{
     const {firstname,lastname,email,password}=req.body;
+    var hashedPassword=await bcrypt.hash(password,10);
     try{
         const newCustomer=new Signup({
         firstname:firstname,
         lastname:lastname,
         email:email,
-        password:password
+        password:hashedPassword
     });
     newCustomer.save();
-    res.status(201).send("yooo!")
+    res.status(201).send("yooo!");
     console.log("value recived")
 }catch(e){
     res.status(401).send("yooo!")
     console.log("unSuccessful")
+
 }
 })
 app.post("/login", async (req, res) => {
@@ -51,19 +55,23 @@ app.post("/login", async (req, res) => {
         if (!user) {
             return res.status(404).json({ message: "User not found" });
         }
-        const user_password=user.password;
-        if (password!=user_password) {
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
             return res.status(401).json({ message: "Invalid password" });
         }
         
-        res.status(200).json({ message: "Login successful", user });
+        res.status(201).json({ message: "Login successful", user });
     } catch (error) {
         console.error(error);
         res.status(500).json({ message: "Internal server error" });
     }
 });
 
-
+app.post("/update",async(req,res)=>{
+    const user=await Signup.findOneAndUpdate({firstname:"luffy"},{$set:{firstname:"Monkey D Luffy"}});
+    res.json("record updated");
+    user.save();
+});
 app.listen(3001,()=>{
     console.log("server is started");
     
